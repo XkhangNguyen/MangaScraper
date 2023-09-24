3300b495bf85101d341c2474cb1f06faa86c6500
Khang <xkhang.nguyen2bme@gmail.com>
2023-09-24 17:17:32 +0700

    Remove unnecessary commented codes

--- browser.js
+++ browser.js
@@ -1,41 +1,23 @@
-import  puppeteer  from 'puppeteer-extra';
-import StealthPlugin  from 'puppeteer-extra-plugin-stealth';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 puppeteer.use(StealthPlugin())
 
-//  async function startBrowserWithProxy(proxyServer){
-// 	let browserWithProxy;
-// 	try {
-// 	    console.log(`Opening the browser with ${proxyServer} ......`);
-// 	    browserWithProxy = await launch({
-// 	        headless: 'new',
-// 	        args: [
-// 				"--disable-setuid-sandbox",
-// 				`--proxy-server=${proxyServer}`,
-// 			],
-// 	        'ignoreHTTPSErrors': true
-// 	    });
-// 	} catch (err) {
-// 	    console.log("Could not create a browser instance => : ", err);
-// 	}
-// 	return browserWithProxy;
-// }
-
-async function startBrowser(){
+async function startBrowser() {
 	let browser;
 	try {
-	    console.log(`Opening the browser with no proxy ......`);
-	    browser = await puppeteer.launch({
-	        headless: 'new',
-	        args: [
+		console.log(`Opening the browser ......`);
+		browser = await puppeteer.launch({
+			headless: 'new',
+			args: [
 				"--disable-setuid-sandbox",
 			],
-	        'ignoreHTTPSErrors': true
-	    });
+			'ignoreHTTPSErrors': true
+		});
 	} catch (err) {
-	    console.log("Could not create a browser instance => : ", err);
+		console.log("Could not create a browser instance => : ", err);
 	}
 	return browser;
 }
 
-export {startBrowser };
+export { startBrowser };
--- pageController.js
+++ pageController.js
@@ -1,21 +1,15 @@
 import { mangaScraperObject } from './pageScraper.js';
-//import { proxiesScraperObject } from './proxyRotator.js';
-//import  {startBrowserWithProxy}  from './browser.js';
 
-async function scrapeAll(browserInstance){
+async function scrapeAll(browserInstance) {
 	let browser;
-	try{
+	try {
 		browser = await browserInstance;
 
-		//const proxy = await proxiesScraperObject.scraper(browser);
-
-		//let browserWithProxy = await startBrowserWithProxy(proxy);
-
 		await mangaScraperObject.scraper(browser);
-			
+
 		await browser.close();
 	}
-	catch(err){
+	catch (err) {
 		console.log("Could not resolve the browser instance => ", err);
 	}
 }
